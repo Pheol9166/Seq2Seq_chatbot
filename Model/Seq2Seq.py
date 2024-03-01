3f54b59ce85eb334b558fc9d93c6b20f2fd02697
Pheol9166 <ksm031216@gmail.com>
2024-03-01 06:22:24 +0000

    organize code

--- Model/Seq2Seq.py
+++ Model/Seq2Seq.py
@@ -4,26 +4,27 @@ import random
 
 
 class Seq2Seq(nn.Module):
-    def __init__(self, encoder, decoder):
-        super(Seq2Seq, self).__init__()
-        self._encoder = encoder
-        self._decoder = decoder
 
-    def forward(self, data, target, teacher_forcing_ratio=0.5):
-        batch_size = target.shape[1]
-        max_len = target.shape[0]
-        vocab_size = self.decoder.ouput_dim
+  def __init__(self, encoder, decoder):
+    super(Seq2Seq, self).__init__()
+    self._encoder = encoder
+    self._decoder = decoder
 
-        outputs = torch.zeros(max_len, batch_size, vocab_size).to(data.device)
-        encoder_outputs, hidden = self._encoder(data)
+  def forward(self, data, target, teacher_forcing_ratio=0.5):
+    batch_size = target.shape[1]
+    max_len = target.shape[0]
+    vocab_size = self.decoder.ouput_dim
 
-        inputs = target[0, :]
+    outputs = torch.zeros(max_len, batch_size, vocab_size).to(data.device)
+    encoder_outputs, hidden = self._encoder(data)
 
-        for t in range(1, max_len):
-            outputs, hidden = self._decoder(inputs, hidden, encoder_outputs)
-            outputs[t] = outputs
-            teacher_force = random.random() < teacher_forcing_ratio
-            top1 = outputs.argmax(1)
-            inputs = target[t] if teacher_force else top1
+    inputs = target[0, :]
 
-        return outputs
+    for t in range(1, max_len):
+      outputs, hidden = self._decoder(inputs, hidden, encoder_outputs)
+      outputs[t] = outputs
+      teacher_force = random.random() < teacher_forcing_ratio
+      top1 = outputs.argmax(1)
+      inputs = target[t] if teacher_force else top1
+
+    return outputs
