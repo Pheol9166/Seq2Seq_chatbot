99aaa18f4cbbc47f4c523f8503d86f0a30e6739c
Sangmin Kim <112241898+Pheol9166@users.noreply.github.com>
2024-03-09 09:02:09 +0000

    modify encoder batch_first setting

--- Model/Encoder.py
+++ Model/Encoder.py
@@ -6,7 +6,10 @@ class Encoder(nn.Module):
   def __init__(self, input_dim, embedding_dim, hidden_dim, num_layers=1):
     super(Encoder, self).__init__()
     self._embedding = nn.Embedding(input_dim, embedding_dim)
-    self._lstm = nn.LSTM(embedding_dim, hidden_dim, num_layers=num_layers)
+    self._lstm = nn.LSTM(embedding_dim,
+                         hidden_dim,
+                         num_layers=num_layers,
+                         batch_first=True)
 
   def forward(self, x):
     embed = self._embedding(x)
